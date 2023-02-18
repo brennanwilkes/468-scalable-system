b7ff85bd1209ec3d08cc1a69ffd6d2a8396c2034
Brennan Wilkes <brennan@codexwilkes.com>
2023-02-17 17:00:55 -0800

    feat: Basic webserver setup

---
+++ webserver/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express';
+import { apiRouter } from './routes/api';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use('/api', apiRouter);
+
+
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: 'Page not found!' });
+});
+
+const port: number | string = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
