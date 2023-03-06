c53033a9bab6b735280c7bf556e22d4fe63fec01
James Kaberry <jameslkaberry@uvic.ca>
2023-03-06 14:50:27 -0800

    Forgot to add change to prev commit

--- transaction-server/src/mongoTypes/User.ts
+++ transaction-server/src/mongoTypes/User.ts
@@ -6,8 +6,8 @@ export interface UserMongo {
     username: string;
     account_balance: number;
     stocks_owned: {stock_name: string, amount: number}[];
-    pending_buy?: {stock_name: string, stock_price: number; amount_to_buy: number, timestamp: number};
-    pending_sell?: {stock_name: string, stock_price: number; amount_to_sell: number, timestamp: number};
+    pending_buy?: {stock_name: string, stock_price: number; amount_to_buy: number, timestamp: number, cryptoKey: string};
+    pending_sell?: {stock_name: string, stock_price: number; amount_to_sell: number, timestamp: number, cryptoKey: string};
     account_balance_reserves: {stock_name: string, amount_in_reserve: number, timestamp: number}[]; //For SET_BUY_AMOUNT
     stocks_owned_reserves: {stock_name: string, amount_in_reserve: number, timestamp: number}[]; //Fpr SET_SELL_AMOUNT
     created: number;
